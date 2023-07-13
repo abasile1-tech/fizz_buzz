ed8451d68ade698bc8d4b660037925f1ca3c6f36
John Basile <ashton.basile@gmail.com>
2023-07-13 13:32:32 +0100

    I wrote some tests first as part of test-driven development

---
+++ run_tests.py
@@ -0,0 +1,5 @@
+import unittest
+from tests.fizz_buzz_test import TestFizzBuss
+
+if __name__ == '__main__':
+	unittest.main
---
+++ tests/fizz_buzz_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.fizz_buzz import fizz_buzz
+
+class TestFizzBuss(unittest.TestCase):
+	def test_fizz(self):
+		self.assertEqual("Fizz", fizz_buzz(3))
+	def test_buzz(self):
+		self.assertEqual("Buzz", fizz_buzz(5))
+	def test_fizz_buzz(self):
+		self.assertEqual("FizzBuzz", fizz_buzz(15))
+	def test_other(self):
+		self.assertEqual("7", fizz_buzz(7))
